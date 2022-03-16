2a0fafef1b7f6b9d43ca00dc0f9ac5ffa607c43e
is41-grigorevps <pavel51393@gmail.com>
2022-03-16 13:52:35 +0500

    aye

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-const DB_HOSTNAME = '89.108.83.110';
-const DB_USERNAME = 'is342_eremin';
-const DB_PASSWORD = 'testingmysql';
-const DB_DATABASE = 'is342_eremin';
+const DB_HOSTNAME = 'mysqlis.radiotech.su.';
+const DB_USERNAME = 'is341_4_pashka';
+const DB_PASSWORD = '9oRoDqHkx7';
+const DB_DATABASE = 'is341_4_Grigorev';
 const DB_PORT = '3306'; # Default 3306
