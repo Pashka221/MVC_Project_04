79cf11a679c031ea103ca7da93a61d7885118aaa
is41-grigorevps <pavel51393@gmail.com>
2022-03-19 11:41:08 +0500

    Version 1.0.6 Work

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 const DB_HOSTNAME = 'mysqlis.radiotech.su.';
-const DB_USERNAME = 'is341_4_pashka';
-const DB_PASSWORD = '9oRoDqHkx7';
+const DB_USERNAME = 'is341_4_4_pashka';
+const DB_PASSWORD = 'HTV7yTW5pZ';
 const DB_DATABASE = 'is341_4_Grigorev';
 const DB_PORT = '3306'; # Default 3306
