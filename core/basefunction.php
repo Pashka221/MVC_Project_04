402dcb3c88c2ca9d168d50a6fe9443559a538bd1
is41-grigorevps <pavel51393@gmail.com>
2022-03-19 14:54:44 +0500

    Version 1.0.7 - Step 3.5

---
+++ core/basefunction.php
@@ -0,0 +1,10 @@
+<?php
+function session($name){
+        return $_SESSION[$name];
+}
+function has_session($name){
+    return isset($_SESSION[$name]);
+}
+function put_session($name,$value){
+    $_SESSION[$name]=$value;
+}
--- core/view.php
+++ core/view.php
@@ -1,12 +1,11 @@
 <?php
 function view($name, $arg = []) {
     ob_start();
-    if (isset($arg)&&$arg !=[])
-        foreach ($arg as $key => $value){
-             $$key = $value;
-
+    # Создаем переменные из названия ключей с использованием операции переменные переменных
+    if(isset($arg) && $arg != [])
+        foreach($arg as $key => $value) {
+            $$key = $value;
         }
-
     # Создание переменной error для записей ошибок
     $error = ($arg['errors'] ?? []);
 
@@ -54,7 +53,6 @@ function view($name, $arg = []) {
                 </div>';
         }
     };
-
 
     include_once 'views/' . $name . '.php';
     $content = ob_get_contents();
